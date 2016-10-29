66bf394a5d58542a9bbffff13a9b6205b19a43e7
Sergei Marochkin <xziggix@gmail.com>
2016-10-29 14:08:48 +0300

    Fix latest changes

--- compiler/sc1.c
+++ compiler/sc1.c
@@ -28,7 +28,6 @@
 #include <assert.h>
 #include <ctype.h>
 #include <limits.h>
-#include <process.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -37,6 +36,7 @@
 #if defined __WIN32__ || defined _WIN32 || defined __MSDOS__
   #include <conio.h>
   #include <io.h>
+  #include <process.h>
 #endif
 #if defined __GNUC__ || defined __clang__
   #include <unistd.h>
@@ -642,11 +642,13 @@ int pc_compile(int argc, char *argv[])
         if (makestategraph) {
           /* run stategraph to create a dot file */
           char dotname[_MAX_PATH];
-          char pgmname[_MAX_PATH];
           strcpy(dotname,reportname);
           set_extension(dotname,".dot",TRUE);
-          sprintf(pgmname,"%s%cstategraph.exe",sc_binpath,DIRSEP_CHAR);
-          spawnl(P_WAIT,pgmname,pgmname,reportname,dotname,NULL);
+          #if defined __WIN32__ || defined _WIN32 || defined __MSDOS__
+            char pgmname[_MAX_PATH];
+            sprintf(pgmname,"%s%cstategraph.exe",sc_binpath,DIRSEP_CHAR);
+            spawnl(P_WAIT,pgmname,pgmname,reportname,dotname,NULL);
+          #endif
         }
       } /* if */
       if (pc_globaldoc!=NULL) {
