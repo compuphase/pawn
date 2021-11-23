eca50b0ef028593098d2850b6dcd904a7e73d0bd
Zoltán Böszörményi <zboszor@gmail.com>
2021-11-23 07:43:10 +0100

    Allow using mkdir(), rmdir() and stat() for Linux and BSD
    This fixes implicit declaration warnings.
    
    Signed-off-by: Zoltán Böszörményi <zboszor@gmail.com>

--- amx/amxfile.c
+++ amx/amxfile.c
@@ -93,7 +93,7 @@
   #define _tgetenv      getenv
   #define _tremove      remove
   #define _trename      rename
-  #if defined __APPLE__
+  #if defined __LINUX__ || defined __FreeBSD__ || defined __OpenBSD__ || defined __APPLE__
     #define _tmkdir     mkdir
     #define _trmdir     rmdir
     #define _tstat      stat
